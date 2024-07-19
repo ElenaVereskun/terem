b55bd0cb8151bcaa5f58a8c7b7aef8e8a744d6f9
ElenaVereskun <lena.vereskun@yandex.ru>
2024-07-19 16:22:54 +0300

    [*]fix js

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
-//закрытие модального
-$(document).ready(function () {
-  $(".btn-secondary").click(function () {
-    $(".modal").removeClass("modal-open");
-  });
-});
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .querySelector(".btn-secondary")
+    .addEventListener("click", function () {
+      document.querySelector(".modal").classList.remove("modal-open");
+    });
 
-//кнопка 1
-$(document).ready(function () {
-  $(".btn-warning").click(function () {
-    $(".section-title").toggleClass("section-title_none");
-  });
-});
+  document.querySelector(".btn-warning").addEventListener("click", function () {
+    const title = document.querySelector(".title");
 
-//кнопка 2
-$(document).ready(function () {
-  $(".btn-success").click(function () {
-    $(".border-dotted").toggleClass("section-middle_first");
+    if (title.classList.contains("title_none")) {
+      title.classList.remove("title_none");
+    } else {
+      title.classList.add("title_none");
+    }
+  });
+  document.querySelector(".btn-success").addEventListener("click", function () {
+    const middleBlock = document.querySelector(".border-dotted");
+
+    if (middleBlock.classList.contains("middle_first")) {
+      middleBlock.classList.remove("middle_first");
+    } else {
+      middleBlock.classList.add("middle_first");
+    }
   });
 });
